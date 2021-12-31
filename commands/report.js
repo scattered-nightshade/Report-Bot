6dc045dc2659465762d3af6b7bd31dce129f8bf5
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-12-31 00:59:33 +0000

    Fixing a ZodError

--- commands/report.js
+++ commands/report.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { ChannelID } = require('../config.json');
 module.exports = {
+	name: 'report',
 	data: new SlashCommandBuilder()
 		.setName('report')
 		.setDescription('Anonymously report someone for their actions')
-        .addStringOption(option => option.setName('Report').setDescription('Report someone or an action, please be descriptive').setRequired(true)),
+		.addStringOption(option => option.setName('report').setDescription('Report someone or something').setRequired(true)),
 	async execute(client, interaction) {
 		await interaction.reply({ content: 'Your report has been sent to staff, thanks for reporting!', ephemeral: true }); // Sends the message but as an ephemeral one
         const reportMessage = interaction.options.getString('Report'); // Gets the message from a string option with the name of 'Report'
--- interactionCommands.js
+++ interactionCommands.js
@@ -1,8 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = [
-        new SlashCommandBuilder()
+	new SlashCommandBuilder()
         .setName('report')
         .setDescription('Anonymously report someone for their actions')
-        .addStringOption(option => option.setName('Report').setDescription('Report someone or an action, please be descriptive').setRequired(true)),
+        .addStringOption(option => option.setName('report').setDescription('Report someone or something').setRequired(true)),
 ];
