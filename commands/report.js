564c70a8935f3224a73055710c476209d8d95614
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-01-02 10:36:50 +0000

    Added heroku capability and changed channel name to channel id

--- commands/report.js
+++ commands/report.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { ChannelID } = require('../config.json');
+const { ReportChanelID } = require('../config.json');
 module.exports = {
 	name: 'report',
 	data: new SlashCommandBuilder()
@@ -8,7 +8,7 @@ module.exports = {
 		.addStringOption(option => option.setName('report').setDescription('Report someone or something').setRequired(true)),
 	async execute(client, interaction) {
         const reportMessage = interaction.options.getString('report'); // Gets the message from a string option with the name of 'Report'
-		interaction.guild.channels.cache.find(channelReports => channelReports.name === 'member-reports').send({ content: `**New report:** ${reportMessage}` });
+		interaction.guild.channels.cache.find(channelReports => channelReports.id === ReportChanelID).send({ content: `**New report:** ${reportMessage}` });
 		await interaction.reply({ content: 'Your report has been sent to staff, thanks for reporting!', ephemeral: true }); // Sends the message but as an ephemeral one
 	},
 };
