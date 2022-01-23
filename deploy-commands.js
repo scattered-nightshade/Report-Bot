3a780a30d649976943f9f73faefbbd0080b78828
PWalkersCrisps <pwalkerscrisps@gmail.com>
2022-01-23 00:23:01 +0000

    Fixing errors that may have caused an issue when hosting

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,7 +4,7 @@ const { readdirSync } = require('fs');
 
 require('dotenv').config();
 
-const { CLIENT_ID, GUILD_ID } = require('./config.json');
+const { ClientID, GuildID, BotToken } = require('./config.json');
 
 const commands = require('./interactionCommands.js'); // Gets all of the commands created in ./interactionCommands.js
 const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js')); // Creates a filter where it only acknowledges files that end with ".js"
@@ -14,10 +14,11 @@ for (const file of commandFiles) { // Loops through all of the commands inside o
 	commands.push(command.data.toJSON());
 }
 
+const DiscordBotToken = process.env.DISCORD_BOT_TOKEN || BotToken; // It first attempts to get the bot token from a .env or enviromental variables (as you should probably put them in there), if not it will then try to find the bot token in ./config.json which is admittedly much less secure
 
 // I honestly dont know how to explain this shit, this might help "https://en.wikipedia.org/wiki/Representational_state_transfer"
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: '9' }).setToken(DiscordBotToken);
 
-rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+rest.put(Routes.applicationGuildCommands(ClientID, GuildID), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
